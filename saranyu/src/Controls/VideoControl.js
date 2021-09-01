083f8729bc8fe370cdb88054e28a92e9b68ea26d
SandeepSaranyu <sandeep.bhat@saranyu.in>
2021-09-01 16:41:12 +0530

    added controls functionality

--- saranyu/src/Controls/VideoControl.js
+++ saranyu/src/Controls/VideoControl.js
@@ -1,10 +1,202 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Grid, IconButton, Menu, MenuItem, Slider, Tooltip, Button, Typography, Popover} from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Forward30, Fullscreen, Pause, PauseCircleOutline, PlayArrow, PlayArrowOutlined, Replay30, VolumeOff, VolumeUp } from '@material-ui/icons';
 
-function VideoControl() {
+const option = ["Configuration", "Settings", "Login"];
+const ITEM_HEIGHT = 48;
+
+
+function VideoControl({playPaus, play,
+                       onForward, onReplay,
+                       onMute, mute,
+                       onVolumeUp, onVoluemDown, volume,
+                       onPlayback, playBack,
+                       onFullScreen,
+                       progress
+                    }) {
+    
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [speedbol, setSpeedbol] = useState(null);
+    const open = Boolean(anchorEl);
+    const openspeed = Boolean(speedbol)
+    const id = openspeed ? 'playbackrate-popover' : undefined;
+    
+
+  const handleClick = (event, data) => {          
+      if(data=== "more"){
+        setAnchorEl(event.currentTarget);
+      }    
+    if(data === "speed"){
+        setSpeedbol(event.currentTarget)
+    }
+  };
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+    setSpeedbol(null);
+  };
+
     return (
-        <div>
-            
+        <div>            
+            {/* Top of the video */}
+            <div className="video-control">
+                <Grid container direction="row" alignItems="center" justifyContent="space-between">
+                    <Grid item>
+                        <p style={{color:"white"}} >Sample video.mp4</p>
+                    </Grid>
+                    <Grid item>
+                        <IconButton
+                            aria-label="more"
+                            aria-controls="menu"
+                            aria-haspopup="true"
+                            onClick={(e)=>handleClick(e,"more")}
+                            // color="primary"
+                            style={{color:"white"}}
+                        >
+                            <MoreVertIcon  />   
+                        </IconButton>                                                                                         
+                        <Menu
+                            id="menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={open}
+                            onClose={handleClose}
+                            PaperProps={{
+                            style: {
+                                maxHeight: ITEM_HEIGHT * 4.5,
+                                width: '20ch',
+                            },
+                            }}
+                        >
+                            {option.map((option) => (
+                            <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+                                {option}
+                            </MenuItem>
+                            ))}
+                        </Menu>
+                    </Grid>
+                </Grid>
+{/* Center control                         */}
+                <Grid container direction="row" alignItems="center" justifyContent="center">
+                    <IconButton onClick={onReplay} className="icon-custom" aria-label="required">
+                        <Replay30 />
+                    </IconButton>
+                    <IconButton onClick={playPaus} className="icon-custom" aria-label="required">
+                      {play? <PauseCircleOutline /> : <PlayArrowOutlined /> }
+                    </IconButton>
+                    <IconButton onClick={onForward} className="icon-custom" aria-label="required">
+                        <Forward30 />
+                    </IconButton>
+                </Grid>
+{/* Bottom control                         */}
+                <Grid container direction="row" justifyContent="space-between" alignItems="center" style={{padding:"10px"}}>
+                    {/* <PrettoSlider min={0} max={100} defaultValue={20} /> */}
+                    <Grid item xs={12}>
+                        <Slider
+                            min={0}
+                            max={100}
+                            style={{color:"#fff"}}
+                            ValueLabelComponent={ValueLabelComponent}
+                            aria-label="custom thumb label"
+                            value = {progress * 100}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Grid container alignItems="center" >
+                            <IconButton onClick={playPaus} className="bottom-play" style={{color:"#fff"}}>
+                               {play? <Pause/> : <PlayArrow/>  }
+                            </IconButton>
+                            <IconButton onClick={onMute} className="bottom-play" style={{color:"#fff"}}>
+                               {mute? <VolumeOff /> : <VolumeUp/>   }
+                            </IconButton>
+                            <span style={{display: "flex"}}>
+                                <Slider 
+                                    min={0} max={100} defaultValue={volume * 100} style={{ width: 100}} 
+                                    onChange={onVolumeUp} onChangeCommitted={onVoluemDown}
+                                    />
+                            </span>                                    
+                            <Button variant="text"className="bottom-play" style={{color:"#fff"}}>
+                                <Typography>05:30</Typography>
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Button onClick={(e)=>handleClick(e, "speed")} variant="text"className="bottom-play" style={{color:"#fff"}}>
+                            <Typography>{playBack}X</Typography>
+                        </Button>
+                        <Popover
+                            id={id}
+                            open={openspeed}
+                            anchorEl={speedbol}
+                            onClose={handleClose}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                                }}
+                            transformOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'center',
+                                }}
+                        >
+                            <Grid container direction="column-reverse">
+                            {[0.5, 1, 1.5, 2].map(e=>
+                            <Button onClick={()=> onPlayback(e)} variant="text">
+                                <Typography color={e === playBack? "primary" : "default"}>{e}X</Typography>
+                            </Button>
+                            )}
+                            </Grid>   
+                        </Popover>
+                        <IconButton onClick={onFullScreen} className="bottom-play" style={{color:"#fff"}}>
+                            <Fullscreen />                                    
+                        </IconButton>
+                    </Grid>
+                </Grid>                        
+            </div>                                    
         </div>
     )
 }
+
+
+function ValueLabelComponent(props) {
+    const { children, open, value } = props;
+  
+    return (
+      <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+        {children}
+      </Tooltip>
+    );
+  }
+
+// const PrettoSlider = withStyles({
+//     root: {
+//       color: 'primary',
+//       height: 8,
+//     },
+//     thumb: {
+//       height: 24,
+//       width: 24,
+//       backgroundColor: '#fff',
+//       border: '2px solid currentColor',
+//       marginTop: -8,
+//       marginLeft: -12,
+//       '&:focus, &:hover, &$active': {
+//         boxShadow: 'inherit',
+//       },
+//     },
+//     active: {},
+//     valueLabel: {
+//       left: 'calc(-50% + 4px)',
+//     },
+//     track: {
+//       height: 8,
+//       borderRadius: 4,
+//     },
+//     rail: {
+//       height: 8,
+//       borderRadius: 4,
+//     },
+//   })(Slider);
+
+
 export default  VideoControl
--- saranyu/src/Player/videoStream.js
+++ saranyu/src/Player/videoStream.js
@@ -1,203 +1,91 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import ReactPlayer from 'react-player'
-import {Container, Grid, IconButton, Menu, MenuItem, Slider, withStyles, Tooltip, Button, Typography, Popover} from '@material-ui/core';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { Forward10, Fullscreen, PauseCircleOutline, PlayArrow, Replay10, VolumeUp } from '@material-ui/icons';
-
-const option = ["Configuration", "Settings", "Login"];
-const ITEM_HEIGHT = 48;
+import {Container} from '@material-ui/core';
+import VideoControl from '../Controls/VideoControl';
+import screenfull from 'screenfull';
 
 
 function VideoStreaming() {
+    const [state, setState] = useState({
+        play: false, mute: false, volume: 1, playBack: 1.0, progress: 0
+    })
+    const playerRef  = useRef(null)
+    const playerContainRef = useRef()
 
-    const [anchorEl, setAnchorEl] = useState(null);
-    const [speedbol, setSpeedbol] = useState(null);
-    const open = Boolean(anchorEl);
-    const openspeed = Boolean(speedbol)
-    const id = openspeed ? 'playbackrate-popover' : undefined;
-    
-
-  const handleClick = (event, data) => {      
-      console.log(event)
-      if(data=== "more"){
-        setAnchorEl(event.currentTarget);
-      }    
-    if(data === "speed"){
-        setSpeedbol(event.currentTarget)
+    const handlePlayPause = () =>{
+        setState({...state, play: !state.play})
     }
-  };
-  
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
+    const handleForword = () =>{
+        playerRef.current.seekTo(playerRef.current.getCurrentTime() + 30)
+    }
+
+    const handleReplay = () =>{
+        playerRef.current.seekTo(playerRef.current.getCurrentTime() - 30)
+    }
+
+    const handleMute = () =>{
+        setState({...state, mute: !state.mute})
+    }
+
+    const handleVolumeUp = (e, update) =>{
+        setState({...state,
+                    volume: parseFloat(update/100),
+                    mute: update === 0? true : false,
+                })                    
+    }
+
+    const handleVolumeDown = (e, update) =>{
+        setState({...state,
+                    volume: parseFloat(update/100),
+                    mute: update === 0? true : false,
+                })                    
+    }
+
+    const handlePlayBackSpeed = (speed) =>{
+        setState({...state,
+                    playBack: speed})
+    }
+
+    const handleFullScreen = () =>{
+        screenfull.toggle(playerContainRef.current)
+    }
+
+    const handleProgress = (changeState) =>{        
+        setState({...state, ...changeState, progress: changeState.played})
+    }
+
+    const {play, mute, volume, playBack, progress} = state;
     return (
         <div>
-            <h1>SARANYU</h1>
+            <span style={{display:"flex", justifyContent:"center"}}><h1>SARANYU</h1></span>            
             <Container maxWidth="md">
-                <div className="video-player">
-                    <ReactPlayer  
-                        playing
-                        url='http://techslides.com/demos/sample-videos/small.mp4'
-                        width='100%'
-                        height='100%'
+                <div ref={playerContainRef} className="video-player">
+                    <ReactPlayer 
+                        ref = {playerRef} 
+                        playing = {play}
+                        muted = {mute}
+                        url = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+                        width = '100%'
+                        height = '100%'
+                        volume = {volume}
+                        playbackRate = {playBack}
+                        onProgress = {handleProgress}    
                         // controls
-                        />
-
-{/* Top of the video */}
-                    <div className="video-control">
-                        <Grid container direction="row" alignItems="center" justifyContent="space-between">
-                            <Grid item>
-                                <p style={{color:"white"}} >Sample video.mp4</p>
-                            </Grid>
-                            <Grid item>
-                                <IconButton
-                                    aria-label="more"
-                                    aria-controls="menu"
-                                    aria-haspopup="true"
-                                    onClick={(e)=>handleClick(e,"more")}
-                                    // color="primary"
-                                    style={{color:"white"}}
-                                >
-                                    <MoreVertIcon  />   
-                                </IconButton>                                                                                         
-                                <Menu
-                                    id="menu"
-                                    anchorEl={anchorEl}
-                                    keepMounted
-                                    open={open}
-                                    onClose={handleClose}
-                                    PaperProps={{
-                                    style: {
-                                        maxHeight: ITEM_HEIGHT * 4.5,
-                                        width: '20ch',
-                                    },
-                                    }}
-                                >
-                                    {option.map((option) => (
-                                    <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
-                                        {option}
-                                    </MenuItem>
-                                    ))}
-                                </Menu>
-                            </Grid>
-                        </Grid>
-{/* Center control                         */}
-                        <Grid container direction="row" alignItems="center" justifyContent="center">
-                            <IconButton className="icon-custom" aria-label="required">
-                                <Replay10 />
-                            </IconButton>
-                            <IconButton className="icon-custom" aria-label="required">
-                                <PauseCircleOutline />
-                            </IconButton>
-                            <IconButton className="icon-custom" aria-label="required">
-                                <Forward10 />
-                            </IconButton>
-                        </Grid>
-{/* Bottom control                         */}
-                        <Grid container direction="row" justifyContent="space-between" alignItems="center" style={{padding:"10px"}}>
-                            {/* <PrettoSlider min={0} max={100} defaultValue={20} /> */}
-                            <Grid item xs={12}>
-                                <Slider
-                                    style={{color:"#fff"}}
-                                    ValueLabelComponent={ValueLabelComponent}
-                                    aria-label="custom thumb label"
-                                    defaultValue={20}
-                                />
-                            </Grid>
-                            <Grid item>
-                                <Grid container alignItems="center" >
-                                    <IconButton className="bottom-play" style={{color:"#fff"}}>
-                                        <PlayArrow/>
-                                    </IconButton>
-                                    <IconButton className="bottom-play" style={{color:"#fff"}}>
-                                        <VolumeUp/>                                    
-                                    </IconButton>
-                                    <span style={{display: "flex"}}>
-                                        <Slider min={0} max={100} defaultValue={90} style={{ width: 100}} />
-                                    </span>                                    
-                                    <Button variant="text"className="bottom-play" style={{color:"#fff"}}>
-                                        <Typography>05:30</Typography>
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                            <Grid item>
-                                <Button onClick={(e)=>handleClick(e, "speed")} variant="text"className="bottom-play" style={{color:"#fff"}}>
-                                    <Typography>1X</Typography>
-                                </Button>
-                                <Popover
-                                    id={id}
-                                    open={openspeed}
-                                    anchorEl={speedbol}
-                                    onClose={handleClose}
-                                    anchorOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'center',
-                                        }}
-                                    transformOrigin={{
-                                        vertical: 'bottom',
-                                        horizontal: 'center',
-                                        }}
-                                >
-                                 <Grid container direction="column-reverse">
-                                 {[0.5, 1, 1.5, 2].map(e=>
-                                    <Button variant="text">
-                                        <Typography color='secondary'>{e}</Typography>
-                                    </Button>
-                                    )}
-                                 </Grid>   
-                                </Popover>
-                                <IconButton className="bottom-play" style={{color:"#fff"}}>
-                                    <Fullscreen />                                    
-                                </IconButton>
-                            </Grid>
-                        </Grid>                        
-                    </div>                        
-                </div>            
+                        />                    
+                <VideoControl 
+                    playPaus={handlePlayPause} play={play} 
+                    onForward = {handleForword} onReplay = {handleReplay}
+                    onMute = {handleMute} mute = {mute}
+                    onVolumeUp = {handleVolumeUp} onVolumeDown = {handleVolumeDown} volume = {volume}
+                    onPlayback = {handlePlayBackSpeed} playBack = {playBack}
+                    onFullScreen = {handleFullScreen}
+                    progress = {progress}
+                />     
+                </div>                       
             </Container>            
         </div>
     )
 }
-
-
-function ValueLabelComponent(props) {
-    const { children, open, value } = props;
-  
-    return (
-      <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
-        {children}
-      </Tooltip>
-    );
-  }
-
-const PrettoSlider = withStyles({
-    root: {
-      color: 'primary',
-      height: 8,
-    },
-    thumb: {
-      height: 24,
-      width: 24,
-      backgroundColor: '#fff',
-      border: '2px solid currentColor',
-      marginTop: -8,
-      marginLeft: -12,
-      '&:focus, &:hover, &$active': {
-        boxShadow: 'inherit',
-      },
-    },
-    active: {},
-    valueLabel: {
-      left: 'calc(-50% + 4px)',
-    },
-    track: {
-      height: 8,
-      borderRadius: 4,
-    },
-    rail: {
-      height: 8,
-      borderRadius: 4,
-    },
-  })(Slider);
   
 export default VideoStreaming
